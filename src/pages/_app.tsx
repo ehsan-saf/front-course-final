789ba8800ac18ff323ed2c711de4999d5455de05
M.Ehsan Safiei <ehsansf2727@gmail.com>
2025-08-01 11:35:00 +0330

    Create basic layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
+import Layout from "@/components/layouts/Layout";
 import "@/styles/globals.css";
 import "@/styles/uicons-regular-straight.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />;
+    </Layout>
+  );
 }
