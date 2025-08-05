e1602114cd7993f5ca345b1f9cc48cdf2324568a
M.Ehsan Safiei <ehsansf2727@gmail.com>
2025-08-05 18:32:58 +0330

    Load fonts using next /font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,42 @@ import { Layout } from "@/components";
 import "@/styles/globals.css";
 import "@/styles/uicons-regular-straight.css";
 import type { AppProps } from "next/app";
+import { Quicksand, Lato, Montserrat } from "next/font/google";
+
+const quicksand = Quicksand({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "700",
+  variable: "--font-quicksand",
+});
+
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-lato",
+});
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["500"],
+  variable: "--font-montserrat",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <style jsx global>{`
+        html {
+          font-family: ${quicksand.style.fontFamily}, sans-serif;
+          --font-quicksand: ${quicksand.style.fontFamily}, sans-serif;
+          --font-lato: ${lato.style.fontFamily};
+          --font-montserrat: ${montserrat.style.fontFamily};
+        }
+
+        p {
+          font-family: "montserrat";
+        }
+      `}</style>
       <Component {...pageProps} />;
     </Layout>
   );
