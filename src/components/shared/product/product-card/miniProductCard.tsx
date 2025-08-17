e5aa518a736c5ba62f485e580ce5a7d9b36dc771
M.Ehsan Safiei <ehsansf2727@gmail.com>
2025-08-17 07:34:44 +0330

    Adjust mini card image size

--- src/components/shared/product/product-card/miniProductCard.tsx
+++ src/components/shared/product/product-card/miniProductCard.tsx
@@ -1,5 +1,6 @@
 import { Product } from "@/types/Product";
 import { ImageView, RatingStars, ProductPrice } from "@/components";
+import Link from "next/link";
 
 interface Props {
   data: Product;
@@ -8,17 +9,21 @@ interface Props {
 export function MiniProductCard({ data }: Props) {
   return (
     <div className="flex items-center gap-4 md:gap-6">
-      <ImageView
-        wrapperClassName="max-h-[88px] max-w-[88px] rounded-xl md:max-h-[120px] md:max-w-[120px]"
-        width={120}
-        height={120}
-        src={data.image}
-        alt=""
-      />
+      <Link href={"#"}>
+        <ImageView
+          wrapperClassName="max-[88px] max-[88px] rounded-xl md:h-[120px] md:w-[120px]"
+          width={120}
+          height={120}
+          src={data.image}
+          alt=""
+        />
+      </Link>
       <div className="flex max-w-40 flex-col gap-1 md:max-w-60">
-        <h4 className="ellipsis-2 overflow-hidden text-xs text-ellipsis whitespace-nowrap md:text-base md:whitespace-normal">
-          {data.title}
-        </h4>
+        <Link href={"#"}>
+          <h4 className="ellipsis-2 overflow-hidden text-xs text-ellipsis whitespace-nowrap md:text-base md:whitespace-normal">
+            {data.title}
+          </h4>
+        </Link>
         <RatingStars rating={data.rating} />
         <ProductPrice price={data.price} sale_price={data.sale_price} />
       </div>
