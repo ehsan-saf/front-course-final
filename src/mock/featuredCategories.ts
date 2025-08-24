466bef41f5f604b5f11d961dc05d74479cb3bcf5
M.Ehsan Safiei <ehsansf2727@gmail.com>
2025-08-24 18:52:51 +0330

    Add mock types

---
+++ src/mock/featuredCategories.ts
@@ -0,0 +1,73 @@
+export interface FeaturedCategoryMockType {
+  title: string;
+  img: string;
+  link: string;
+  color: string;
+  items: number;
+}
+
+export const FeaturedCategoriesMock: Array<FeaturedCategoryMockType> = [
+  {
+    title: "Sandwich",
+    img: "/images/cat-13.png",
+    link: "/",
+    color: "#F2FCE4",
+    items: 14,
+  },
+  {
+    title: "Peach",
+    img: "/images/cat-11.png",
+    link: "/",
+    color: "#FFFCEB",
+    items: 17,
+  },
+  {
+    title: "Organic Kiwi",
+    img: "/images/cat-12.png",
+    link: "/",
+    color: "#F2FCE4",
+    items: 21,
+  },
+  {
+    title: "Red Apple",
+    img: "/images/cat-9.png",
+    link: "/",
+    color: "#FEEFEA",
+    items: 68,
+  },
+  {
+    title: "Snack",
+    img: "/images/cat-3.png",
+    link: "/",
+    color: "#FFF3EB",
+    items: 34,
+  },
+  {
+    title: "Black plum",
+    img: "/images/cat-4.png",
+    link: "/",
+    color: "#FFF3FF",
+    items: 25,
+  },
+  {
+    title: "Vegetables",
+    img: "/images/cat-1.png",
+    link: "/",
+    color: "#F2FCE4",
+    items: 65,
+  },
+  {
+    title: "Headphone",
+    img: "/images/cat-15.png",
+    link: "/",
+    color: "#FFFCEB",
+    items: 33,
+  },
+  {
+    title: "Cake & Milk",
+    img: "/images/cat-14.png",
+    link: "/",
+    color: "#F2FCE4",
+    items: 54,
+  },
+];
---
+++ src/mock/products.ts
@@ -0,0 +1,122 @@
+export interface ProductMockType {
+  category?: string;
+  title: string;
+  rating?: number;
+  weight?: number;
+  unit?: string;
+  price: number;
+  sale_price?: number;
+  label?: string;
+  image?: string;
+}
+
+export const popularProductsMock: Array<ProductMockType> = [
+  {
+    category: "Hodo Foods",
+    title: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
+    rating: 4,
+    weight: 500,
+    unit: "gram",
+    price: 2.51,
+    sale_price: 2,
+    image: "/images/18.jpg",
+  },
+  {
+    category: "Hodo Foods",
+    title: "All Natural Italian-Style Chicken Meatballs",
+    rating: 4,
+    weight: 500,
+    unit: "gram",
+    price: 2.51,
+    sale_price: 2,
+    label: "Hot",
+    image: "/images/1.png",
+  },
+  {
+    category: "Hodo Foods",
+    title: "Angie’s Boomchickapop Sweet & Salty Kettle Corn",
+    rating: 4,
+    weight: 500,
+    unit: "gram",
+    price: 2.51,
+    sale_price: 2,
+    label: "Sale",
+    image: "/images/3.png",
+  },
+  {
+    category: "Hodo Foods",
+    title: "Foster Farms Takeout Crispy Classic Buffalo Wings",
+    rating: 4,
+    weight: 500,
+    unit: "gram",
+    price: 2.51,
+    sale_price: 2,
+    image: "/images/5.png",
+  },
+  {
+    category: "Hodo Foods",
+    title: "Blue Diamond Almonds Lightly Salted Vegetables",
+    rating: 4,
+    weight: 500,
+    unit: "gram",
+    price: 2.51,
+    sale_price: 2,
+    image: "/images/7.png",
+  },
+];
+
+// -----------------------------------------------------
+
+export const popularFruitsMock: Array<ProductMockType> = [
+  {
+    category: "Hodo Foods",
+    title: "Blue Diamond Almonds Lightly Salted Vegetables",
+    rating: 4,
+    weight: 500,
+    unit: "gram",
+    price: 2.51,
+    sale_price: 2,
+    image: "/images/7.png",
+  },
+  {
+    category: "Hodo Foods",
+    title: "Seeds of Change Organic Quinoa, Brown, & Red Rice",
+    rating: 4,
+    weight: 500,
+    unit: "gram",
+    price: 2.51,
+    sale_price: 2,
+    image: "/images/1.png",
+  },
+  {
+    category: "Hodo Foods",
+    title: "All Natural Italian-Style Chicken Meatballs",
+    rating: 4,
+    weight: 500,
+    unit: "gram",
+    price: 2.51,
+    sale_price: 2,
+    label: "Sale",
+    image: "/images/3.png",
+  },
+  {
+    category: "Hodo Foods",
+    title: "Angie’s Boomchickapop Sweet & Salty Kettle Corn",
+    rating: 4,
+    weight: 500,
+    unit: "gram",
+    price: 2.51,
+    sale_price: 2,
+    image: "/images/5.png",
+  },
+  {
+    category: "Hodo Foods",
+    title: "Blue Diamond Almonds Lightly Salted Vegetables",
+    rating: 4,
+    weight: 500,
+    unit: "gram",
+    price: 2.51,
+    sale_price: 2,
+    image: "/images/7.png",
+  },
+];
