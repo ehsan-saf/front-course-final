197a60a2ea23973064f9383d360b2ee3dabb5e87
M.Ehsan Safiei <ehsansf2727@gmail.com>
2025-09-26 07:06:11 +0330

    Finish Description component(mock data)

--- src/components/pages/productPage/description-tab/descriptionTab.tsx
+++ src/components/pages/productPage/description-tab/descriptionTab.tsx
@@ -24,14 +24,65 @@ export function DescriptionTab({ data }: Props) {
         more as this less much amid overhung anathematic because much held one
         exuberantly sheep goodness so where rat wry well concomitantly.
       </p>
-      <table className="text-body">
+      <table className="w-fit border-separate border-spacing-3 font-lato text-sm text-body">
         <tr>
-          <td>Type Of Packing</td>
+          <th scope="row" className="text-left font-normal">
+            Type Of Packing
+          </th>
+          <td>Paper wrapping</td>
         </tr>
         <tr>
-          <td>Type Of Packing</td>
+          <th scope="row" className="text-left font-normal">
+            Color
+          </th>
+          <td>Green, Pink, Brown</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-left font-normal">
+            Quantity Per Case
+          </th>
+          <td>500g</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-left font-normal">
+            Fat percentage
+          </th>
+          <td>56%</td>
+        </tr>
+        <tr>
+          <th scope="row" className="text-left font-normal">
+            Piece In One
+          </th>
+          <td>Carton</td>
         </tr>
       </table>
+
+      <p className="text-body">
+        Scallop or far crud plain remarkably far by thus far iguana lewd
+        precociously and and less rattlesnake contrary caustic wow this near
+        alas and next and pled the yikes articulate about as less cackled
+        dalmatian in much less well jeering for the thanks blindly sentimental
+        whimpered less across objectively fanciful grimaced wildly some wow and
+        rose jeepers outgrew lugubrious luridly irrationally attractively
+        dachshund.
+      </p>
+      <h3 className="text-xl">Suggested Use</h3>
+      <p className="text-body">
+        Refrigeration not necessary.
+        <br /> Stir before serving
+      </p>
+      <h3 className="text-xl">Other Ingredients</h3>
+      <p className="text-body">
+        Organic raw pecans, organic raw cashews.
+        <br /> This butter was produced using a LTG (Low Temperature Grinding)
+        process.
+        <br /> Made in machinery that processes tree nuts but does not process
+        peanuts, gluten, dairy or soy.
+      </p>
+      <h3 className="text-xl">Warnings</h3>
+      <p className="text-body">
+        Oil separation occurs naturally. May contain pieces of shell.
+      </p>
     </div>
   );
 }
