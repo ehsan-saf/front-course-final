43a67dfc2dcd5e6744e0ed0a554db31fd9269639
M.Ehsan Safiei <ehsansf2727@gmail.com>
2025-10-06 18:06:02 +0330

    Change price range style

--- src/components/pages/categoryPage/itemFilter/itemFilter.tsx
+++ src/components/pages/categoryPage/itemFilter/itemFilter.tsx
@@ -14,9 +14,9 @@ export function ItemFilter({}: Props) {
       <div className="flex flex-col">
         <h4 className="mb-6 border-b-1 border-b-border pb-3.5">Filter items</h4>
         <div className="flex flex-col gap-6">
-          <div>
-            Price Range:{" "}
-            <span className="text-xl text-brand-1">
+          <div className="flex gap-5 font-lato text-text-muted">
+            <span>Price Range:</span>
+            <span className="font-quicksand text-xl text-brand-1">
               ${priceFilter.minPrice} - ${priceFilter.maxPrice}
             </span>
           </div>
