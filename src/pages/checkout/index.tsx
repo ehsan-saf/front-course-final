180ea369b04e69cb45c3acb1021329a4c942fa57
ehsan <ehsansf2727@gmail.com>
2025-10-28 22:03:17 -0700

    Create payment methods

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,11 +1,16 @@
-import { IconBox } from "@/components";
+import { IconBox, ImageView } from "@/components";
 import { useCart } from "@/hooks";
 import { useUser } from "@/store";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Page() {
   const { user } = useUser();
   const { cartItems } = useCart();
+
+  const [checkedMethod, setCheckedMethod] = useState<"direct" | "on-delivery">(
+    "direct",
+  );
 
   const formatPrice = (price: number) => {
     return price.toFixed(2);
@@ -58,29 +63,91 @@ export default function Page() {
             </div>
           </div>
         </div>
-        {/* Order Items */}
-        <div className="flex-1 rounded-[15px] border border-border p-7">
-          <div className="mb-6 flex justify-between border-b border-b-border pb-7">
-            <h2 className="text-heading">Your Orders</h2>
-            <div className="text-text-muted">Subtotal</div>
+
+        <div className="flex flex-col gap-10">
+          {/* Order Items */}
+          <div className="flex-1 rounded-[15px] border border-border p-7">
+            <div className="mb-6 flex justify-between border-b border-b-border pb-7">
+              <h2 className="text-heading">Your Orders</h2>
+              <div className="text-text-muted">Subtotal</div>
+            </div>
+            <div className="flex flex-col gap-8">
+              {cartItems.map((item, index) => {
+                return (
+                  <div className="grid grid-cols-3" key={index}>
+                    <div className="max-w-[170px]">
+                      {item.product.data.attributes.title}
+                    </div>
+                    <div className="justify-self-center text-text-muted">
+                      X {item.quantity}
+                    </div>
+                    <div className="justify-self-end text-brand-1">
+                      ${getSubtotalPrice(item, true)}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div className="flex flex-col gap-8">
-            {cartItems.map((item, index) => {
-              return (
-                <div className="grid grid-cols-3" key={index}>
-                  <div className="max-w-[170px]">
-                    {item.product.data.attributes.title}
-                  </div>
-                  <div className="justify-self-center text-text-muted">
-                    X {item.quantity}
-                  </div>
-                  <div className="justify-self-end text-brand-1">
-                    ${getSubtotalPrice(item, true)}
-                  </div>
-                </div>
-              );
-            })}
+          <div>
+            <h2 className="mb-7 text-2xl">Payment</h2>
+            <label
+              htmlFor="direct-transfer"
+              className="flex gap-2.5 font-lato text-body has-checked:text-heading"
+            >
+              <input
+                type="radio"
+                id="direct-transfer"
+                name="pay-method"
+                checked={checkedMethod === "direct"}
+                onClick={() => setCheckedMethod("direct")}
+              />
+              Direct bank transfer
+            </label>
+            <label
+              htmlFor="cash-transfer"
+              className="mt-3.5 flex gap-2.5 font-lato text-body has-checked:text-heading"
+            >
+              <input
+                type="radio"
+                id="cash-transfer"
+                name="pay-method"
+                checked={checkedMethod === "on-delivery"}
+                onClick={() => setCheckedMethod("on-delivery")}
+              />
+              Cash on delivery
+            </label>
+            <div className="mt-5 flex items-center gap-2.5">
+              <ImageView
+                src="/icons/paypal.svg"
+                width={80}
+                height={20}
+                wrapperClassName="w-[80px] h-5"
+              />
+              <ImageView
+                src="/icons/visa.svg"
+                width={80}
+                height={20}
+                wrapperClassName="w-[80px] h-6"
+              />
+              <ImageView
+                src="/icons/mastercard.svg"
+                width={80}
+                height={20}
+                wrapperClassName="w-6 h-5"
+              />
+              <ImageView
+                src="/icons/zapper.svg"
+                width={80}
+                height={20}
+                wrapperClassName="w-[80px] h-5"
+              />
+            </div>
           </div>
+          <button className="flex items-center justify-center gap-3 self-stretch rounded-sm bg-brand-1 px-12 py-3.5 text-white md:self-start">
+            <span>Place an Order</span>
+            <IconBox icon="log-in" />
+          </button>
         </div>
       </div>
     </div>
